c4f7a35085dabfcbacfa41cdad74139e5c5e0db5
Kim Cuc Ngo <kimcucngo@Kims-MacBook-Air.local>
2022-11-30 23:45:19 +0900

    fix html

--- 10.php
+++ 10.php
@@ -20,28 +20,21 @@ echo '<hr/>';
 ?>
 <!-- Print multiplicatiom table -->
 <table border = "1" width = "100%">
-    <tr>
         <?php
-        for ($i = 1; $i <= 5; $i++){
+        for ($i = 1; $i <= 10; $i++){
+            if ($i == 1 || $i == 6){
+                echo '<tr>';
+            }
             echo '<td>';
             for ($j = 1; $j <= 10; $j++){
                 echo $i.' x '.$j.' = '.$i*$j.'<br/>';
             }
-        echo '</td>';
-        } 
-        ?>
-    </tr>
-    <tr>
-        <?php
-        for ($i = 6; $i <= 10; $i++){
-            echo '<td>';
-            for ($j = 1; $j <= 10; $j++){
-                echo $i.' x '.$j.' = '.$i*$j.'<br/>';
+            echo '</td>';
+            if ($i == 5 || $i == 10){
+                echo '</tr>';
             }
-        echo '</td>';
         } 
         ?>
-    </tr>
-</table>
+    </table>
 <?php
 echo '<hr/>';
